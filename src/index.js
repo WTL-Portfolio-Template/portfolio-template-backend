4cb48e054cd96666b30f6d497024f732d7c671d4
RohinBhat <bhatrohin003@gmail.com>
2022-04-29 17:27:05 +0530

    Set up CORS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 require("./db/mongoose");
 const messageRouter = require("./routers/message.router");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/messages", messageRouter);
